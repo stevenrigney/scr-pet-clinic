eae6861fbc44940d258b7adc45bb23ea7b70a8fe
Steven Rigney <sr794p@att.com>
2018-08-27 16:25:09 -0500

    Initial commit from start.spring.io no changes have been made yet

---
+++ src/main/java/guru/springframework/scrpetclinic/ScrPetClinicApplication.java
@@ -0,0 +1,12 @@
+package guru.springframework.scrpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScrPetClinicApplication.class, args);
+	}
+}
