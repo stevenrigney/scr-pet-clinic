a3b80b96e5adbcdee99e2b43c37f9f1d3944b449
Rigney, Steven (sr794p) <sr794p@us.att.com>
2018-09-27 18:57:33 -0500

    Added the Pet Type Service interface and map implementation.

---
+++ pet-clinic-data/src/main/java/guru/springframework/scrpetclinic/services/PetTypeService.java
@@ -0,0 +1,7 @@
+package guru.springframework.scrpetclinic.services;
+
+import guru.springframework.scrpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+
+}
