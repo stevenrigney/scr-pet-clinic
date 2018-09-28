07f98e7d859ffc93e80d7f316025b89976a38b9e
Rigney, Steven (sr794p) <sr794p@us.att.com>
2018-09-27 20:34:14 -0500

    Created a SpecialtyService interface and a Map implementation of it.

---
+++ pet-clinic-data/src/main/java/guru/springframework/scrpetclinic/services/SpecialityService.java
@@ -0,0 +1,7 @@
+package guru.springframework.scrpetclinic.services;
+
+import guru.springframework.scrpetclinic.model.Specialty;
+
+public interface SpecialityService extends CrudService<Specialty, Long> {
+
+}
