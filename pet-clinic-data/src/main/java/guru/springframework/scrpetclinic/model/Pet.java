2874113c964c11ea2720785cb3ce025f535a5047
Rigney, Steven (sr794p) <sr794p@us.att.com>
2018-10-04 15:48:06 -0500

    Added JPA mapping for Visit, added a link between Pet and Visit

--- pet-clinic-data/src/main/java/guru/springframework/scrpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/guru/springframework/scrpetclinic/model/Pet.java
@@ -1,11 +1,15 @@
 package guru.springframework.scrpetclinic.model;
 
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -25,6 +29,9 @@ public class Pet extends BaseEntity {
 
 	@Column(name = "birth_date")
 	private LocalDate birthDate;
+
+	@OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+	private Set<Visit> visits = new HashSet<>();
 	
 	public PetType getPetType() {
 		return petType;
@@ -49,5 +56,11 @@ public class Pet extends BaseEntity {
 	}
 	public void setName(String name) {
 		this.name = name;
+	}
+	public Set<Visit> getVisits() {
+		return visits;
+	}
+	public void setVisits(Set<Visit> visits) {
+		this.visits = visits;
 	}
 }
--- pet-clinic-data/src/main/java/guru/springframework/scrpetclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/guru/springframework/scrpetclinic/model/Visit.java
@@ -2,9 +2,24 @@ package guru.springframework.scrpetclinic.model;
 
 import java.time.LocalDate;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "visits")
 public class Visit extends BaseEntity {
+	
+	@Column(name = "visit_date")
 	private LocalDate date;
+	
+	@Column(name = "description")
 	private String description;
+
+	@ManyToOne
+	@JoinColumn(name = "pet_id")
 	private Pet pet;
 	
 	public LocalDate getDate() {
