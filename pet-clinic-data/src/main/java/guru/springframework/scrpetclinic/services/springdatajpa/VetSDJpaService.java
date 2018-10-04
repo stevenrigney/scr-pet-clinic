3f077e60837e831e1373687bbd4733e97175ed8c
Rigney, Steven (sr794p) <sr794p@us.att.com>
2018-10-04 16:29:48 -0500

    Added a Vet Spring data jpa implementation.

---
+++ pet-clinic-data/src/main/java/guru/springframework/scrpetclinic/services/springdatajpa/VetSDJpaService.java
@@ -0,0 +1,52 @@
+package guru.springframework.scrpetclinic.services.springdatajpa;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import guru.springframework.scrpetclinic.model.Vet;
+import guru.springframework.scrpetclinic.repositories.VetRepository;
+import guru.springframework.scrpetclinic.services.VetService;
+
+@Service
+@Profile("springdatajpa")
+public class VetSDJpaService implements VetService {
+
+	private final VetRepository vetRepository;
+	
+	public VetSDJpaService(VetRepository vetRepository) {
+		super();
+		this.vetRepository = vetRepository;
+	}
+
+	@Override
+	public Set<Vet> findAll() {
+		Set<Vet> vets = new HashSet<>();
+		vetRepository.findAll().forEach(vets::add);
+		
+		return vets;
+	}
+
+	@Override
+	public Vet findById(Long id) {
+		return vetRepository.findById(id).orElse(null);
+	}
+
+	@Override
+	public Vet save(Vet object) {
+		return vetRepository.save(object);
+	}
+
+	@Override
+	public void delete(Vet object) {
+		vetRepository.delete(object);
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		vetRepository.deleteById(id);
+	}
+
+}
