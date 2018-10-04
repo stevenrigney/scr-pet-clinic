ec21610c7c7740baffed3631aba68085f75db7c2
Rigney, Steven (sr794p) <sr794p@us.att.com>
2018-10-04 16:41:58 -0500

    Added the Specialty Spring Data JPA implementation

---
+++ pet-clinic-data/src/main/java/guru/springframework/scrpetclinic/services/springdatajpa/SpecialtySDJpaService.java
@@ -0,0 +1,47 @@
+package guru.springframework.scrpetclinic.services.springdatajpa;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import guru.springframework.scrpetclinic.model.Specialty;
+import guru.springframework.scrpetclinic.repositories.SpecialtyRepository;
+import guru.springframework.scrpetclinic.services.SpecialtyService;
+
+public class SpecialtySDJpaService implements SpecialtyService {
+
+	private final SpecialtyRepository specialtyRepsitory;
+	
+	public SpecialtySDJpaService(SpecialtyRepository specialtyRepsitory) {
+		super();
+		this.specialtyRepsitory = specialtyRepsitory;
+	}
+
+	@Override
+	public Set<Specialty> findAll() {
+		Set<Specialty> specialities = new HashSet<>();
+		specialtyRepsitory.findAll().forEach(specialities::add);
+		
+		return specialities;
+	}
+
+	@Override
+	public Specialty findById(Long id) {
+		return specialtyRepsitory.findById(id).orElse(null);
+	}
+
+	@Override
+	public Specialty save(Specialty object) {
+		return specialtyRepsitory.save(object);
+	}
+
+	@Override
+	public void delete(Specialty object) {
+		specialtyRepsitory.delete(object);
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		specialtyRepsitory.deleteById(id);
+	}
+
+}
