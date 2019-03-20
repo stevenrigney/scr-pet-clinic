367043c268fdc34b82e4d860de77aa9bcd3b3c3a
Rigney, Steven (sr794p) <sr794p@us.att.com>
2019-03-20 15:16:44 -0500

    Created a map service for Visit.

---
+++ pet-clinic-data/src/main/java/guru/springframework/scrpetclinic/services/VisitService.java
@@ -0,0 +1,7 @@
+package guru.springframework.scrpetclinic.services;
+
+import guru.springframework.scrpetclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+	
+}
---
+++ pet-clinic-data/src/main/java/guru/springframework/scrpetclinic/services/map/VisitMapService.java
@@ -0,0 +1,52 @@
+package guru.springframework.scrpetclinic.services.map;
+
+import java.util.Set;
+
+import org.springframework.stereotype.Service;
+
+import guru.springframework.scrpetclinic.model.Visit;
+import guru.springframework.scrpetclinic.services.VisitService;
+
+@Service
+public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
+
+	private final VisitService visitService;
+	
+	public VisitMapService(VisitService visitService) {
+		super();
+		this.visitService = visitService;
+	}
+	
+	@Override
+	public Set<Visit> findAll() {
+		return super.findAll();
+	}
+
+	@Override
+	public Visit findById(Long id) {
+		return super.findById(id);
+	}
+
+	@Override
+	public Visit save(Visit visit) {
+
+		if( visit.getPet() == null || visit.getPet().getOwner() == null
+				|| visit.getPet().getId() == null
+				|| visit.getPet().getOwner().getId() == null ) {
+			throw new RuntimeException("Invalid visit");
+		}
+
+		return super.save(visit);
+	}
+
+	@Override
+	public void delete(Visit object) {
+		super.delete(object);
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		super.deleteById(id);
+	}
+
+}
